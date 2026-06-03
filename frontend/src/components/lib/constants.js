b1236c1d11a4c1d4d0c6ecbc36f27f2f7eb036f1
Callmax Solutions Repositories <j.abucejo@callmaxsolutions.com>
2026-06-03 12:58:46 -0400

    Update constants.js

--- frontend/src/components/lib/constants.js
+++ frontend/src/components/lib/constants.js
@@ -1,3 +1,3 @@
-export const SERVER_URL = "http://localhost:5010";
-//export const SERVER_URL = "https://qaportalbackend.cmxph.com";
+//export const SERVER_URL = "http://localhost:5010";
+export const SERVER_URL = "https:/qat-qaportalbackend.cmxph.com";
 //export const SERVER_URL = window.location.origin;
